11216f9f7a0797dd63e0aecf0e925be0098c5480
Adam Leskis <leskis@gmail.com>
2021-11-18 23:15:59 +0000

    lets add authentication

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import requests
 
-BASE_URL = 'https://amazing_api.com/'
+from secrets import SECRET_API_KEY
 
-response = requests.get(BASE_URL).json()
+BASE_URL = 'https://amazing_api.com'
+
+headers = {
+    'content-type': 'application/json',
+    'Authorization': f'Bearer {SECRET_API_KEY}'
+}
+
+response = requests.get(BASE_URL, headers=headers).json()
 
 print(response)
